f7940c9e0d12f2e446ea8872e4f0e35fe1cbe30a
eel-primo <47497416+eel-primo@users.noreply.github.com>
2022-07-01 21:54:05 +0300

    Update ipcheck.py
    Optimized imports, some bugfix. AND ONE DAY WE WILL GET AN OOP

--- ipcheck.py
+++ ipcheck.py
@@ -2,16 +2,13 @@
 # vk.com/vl.hoi0
 # t.me/vl_hoi1
 
-# Code review by cd-con
-
+# Code review by cd-con and eel-primo
 from pif import get_public_ip
-import urllib.request
 from urllib.request import urlopen
-import json
 from json import load
 import os
-import colorama
 from colorama import Fore, Back, Style
+import sys
 
 def header(): # Header
     print(Fore.WHITE + '''
@@ -20,27 +17,26 @@ def header(): # Header
     ██║░░░░░██║░░██║''' + Fore.RED + '''╚██╗''' + Fore.WHITE + '''░''' + Fore.RED + '''██╔╝''' + Fore.WHITE + '''█████╗░░██████╔╝
     ██║░░░░░██║░░██║░''' + Fore.RED + '''╚████╔╝''' + Fore.WHITE + '''░██╔══╝░░██╔══██╗
     ███████╗╚█████╔╝░░''' + Fore.RED + '''╚██╔╝''' + Fore.WHITE + '''░░███████╗██║░░██║
-    ╚══════╝░╚════╝░░░░''' + Fore.RED + '''╚═╝''' + Fore.WHITE + '''░░░╚══════╝╚═╝░░╚═╝\n''' + Fore.WHITE + "-" * 60 + Fore.YELLOW + "\nIP and Phone Number checker by VolodyaHoi\nYour IP Adress: "+ get_public_ip() + "\n" + Fore.CYAN + "=" * 60 + Style.RESET_ALL)
+    ╚══════╝░╚════╝░░░░''' + Fore.RED + '''╚═╝''' + Fore.WHITE + '''░░░╚══════╝╚═╝░░╚═╝\n''' + Fore.WHITE + "-" * 60 + Fore.YELLOW 
+          + "\nIP and Phone Number checker by VolodyaHoi\nYour IP Adress: "+ get_public_ip() + "\n" + Fore.CYAN + "=" * 60 + Style.RESET_ALL)
     print("[" + Fore.RED + "1" + Style.RESET_ALL + "] Check IP Adress")
     print("[" + Fore.RED + "2" + Style.RESET_ALL + "] Check Phone Number")
     print("[" + Fore.RED + "3" + Style.RESET_ALL + "] Exit")
-
     print(Fore.CYAN + "=" * 60)
 
 def clear(): # Clear screen
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def procced_command(): # Proceed user input
-    CMD = int(input(Fore.YELLOW + "[" + Fore.BLUE + ">" + Fore.YELLOW + "]" + Style.RESET_ALL + " Enter Command: "))
+    CMD = input(Fore.YELLOW + "[" + Fore.BLUE + ">" + Fore.YELLOW + "]" + Style.RESET_ALL + " Enter Command: ")
 
-    if CMD == 1: # IP checker
-        getIP = input(Fore.YELLOW + "[" + Fore.BLUE + ">" + Fore.YELLOW + "]" + Style.RESET_ALL + " Enter IP Address: ")
-        url = "https://ipinfo.io/" + getIP + "/json"
-        
+    if CMD == '1': # IP checker
         try:
+            getIP = input(Fore.YELLOW + "[" + Fore.BLUE + ">" + Fore.YELLOW + "]" + Style.RESET_ALL + " Enter IP Address: ")
             url = "https://ipinfo.io/" + getIP + "/json"
-            getInfo = urllib.request.urlopen(url)
-            infoList = json.load(getInfo)
+            
+            getInfo = urlopen(url)
+            infoList = load(getInfo)
 
             print(Fore.CYAN + "-" * 60)
             print(Fore.YELLOW + "[" + Fore.BLUE + "+" + Fore.YELLOW + "]" + Style.RESET_ALL + " IP: ", infoList["ip"])
@@ -53,23 +49,23 @@ def procced_command(): # Proceed user input
             print(Fore.YELLOW + "[" + Fore.BLUE + "+" + Fore.YELLOW + "]" + Style.RESET_ALL + " Timezone: ", infoList["timezone"])
             print(Fore.CYAN + "-" * 60)
             
-            next = input(Fore.GREEN + "Press ENTER to go back...")
+            _ = input(Fore.GREEN + "Press ENTER to go back...")
             
-            main()
+            run()
         
         # Too broad!
         except:
             print(Fore.RED + "[!]" + Style.RESET_ALL + " Invalid IP Address!")
-            next = input(Fore.GREEN + "Press ENTER to go back...")
-            main()
+            _ = input(Fore.GREEN + "Press ENTER to go back...")
+            run()
 
-    elif CMD == 2: # Phone number checker
+    elif CMD == '2': # Phone number checker
         phoneNumber = input(Fore.YELLOW + "[" + Fore.BLUE + ">" + Fore.YELLOW + "]" + Style.RESET_ALL + " Enter phone number: ")
         getInfo = "https://htmlweb.ru/geo/api.php?json&telcod=" + phoneNumber
 
         try:
-            infoPhone = urllib.request.urlopen(getInfo)
-            infoPhone = json.load(infoPhone)
+            infoPhone = urlopen(getInfo)
+            infoPhone = load(infoPhone)
 
             print(Fore.CYAN + "-" * 60)
             print(Fore.YELLOW + "[" + Fore.BLUE + "+" + Fore.YELLOW + "]" + Style.RESET_ALL + " Phone Number: ", "+" + phoneNumber)
@@ -77,32 +73,34 @@ def procced_command(): # Proceed user input
             print(Fore.YELLOW + "[" + Fore.BLUE + "+" + Fore.YELLOW + "]" + Style.RESET_ALL + " Region: ", infoPhone["region"]["english"])
             print(Fore.YELLOW + "[" + Fore.BLUE + "+" + Fore.YELLOW + "]" + Style.RESET_ALL + " District: ", infoPhone["region"]["okrug"])
             print(Fore.YELLOW + "[" + Fore.BLUE + "+" + Fore.YELLOW + "]" + Style.RESET_ALL + " AutoCod: ", infoPhone["region"]["autocod"])
-            print(Fore.YELLOW + "[" + Fore.BLUE + "+" + Fore.YELLOW + "]" + Style.RESET_ALL + " Location: ", infoPhone["0"]["latitude"], infoPhone["0"]["longitude"])
+            print(Fore.YELLOW + "[" + Fore.BLUE + "+" + Fore.YELLOW + "]" + Style.RESET_ALL + " Location: ", infoPhone["0"]["latitude"], 
+                                                                                                            infoPhone["0"]["longitude"])
             print(Fore.YELLOW + "[" + Fore.BLUE + "+" + Fore.YELLOW + "]" + Style.RESET_ALL + " Operator: ", infoPhone["0"]["oper"])
             print(Fore.YELLOW + "[" + Fore.BLUE + "+" + Fore.YELLOW + "]" + Style.RESET_ALL + " Timezone: ", infoPhone["0"]["tz"])
             print(Fore.CYAN + "-" * 60)
 
-            next = input(Fore.GREEN + "Press ENTER to go back...")
-            main()
+            _ = input(Fore.GREEN + "Press ENTER to go back...")
+            run()
             
         # Too broad!
         except:
             print(Fore.RED + "[!]" + Style.RESET_ALL + " Invalid phone number!")
             next = input(Fore.GREEN + "Press ENTER to go back...")
-            main()
+            run()
 
-    elif CMD == 3: # Stop the script
-        exit()
+    elif CMD == '3': # Stop the script
+        sys.exit()
 
     else: # Incorrect input
         print(Fore.RED + "[!]" + Style.RESET_ALL + " Wrong command number!")
-        next = input(Fore.GREEN + "Press ENTER to go back...")
-        main()
+        _ = input(Fore.GREEN + "Press ENTER to go back...")
+        run()
 
-def main():
+def run():
     clear()
     header()
     proceed_command()
-
-main()
+    
 # TODO implement OOP
+if __name__ == "__main__":
+    run()
